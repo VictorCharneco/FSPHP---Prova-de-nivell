48afd9d21da9bf7e3d6f911f76567bda23a8c5be
Victor C.B. <victor.charneco.barba@gmail.com>
2025-11-13 11:57:39 +0100

    Exam files uploaded

---
+++ Vuelo.php
@@ -0,0 +1,38 @@
+<?php
+
+class Vuelo{
+    private string $origen;
+    private string $destino;
+    private int $precio;
+    private string $duracion;
+    private string $companyia;
+
+    public function __construct($origen, $destino, $precio, $duracion, $companyia){
+        $this->origen=$origen;
+        $this->destino=$destino;
+        $this->precio=$precio;
+        $this->duracion=$duracion;
+        $this->companyia=$duracion;
+    }
+
+    public function getDestino():string{
+        return $this->destino;
+    }
+
+    public function getPrecio():int{
+        return $this->precio;
+    }
+
+    public function __toString(){
+        return "<b>INFO VUELO:</b><br>
+                Origen: $this->origen <br>
+                Destino: $this->destino <br>
+                Precio: $this->precio <br>
+                Duración: $this->duracion <br>
+                Compañía: $this->companyia <br>";
+    }
+
+}
+
+
+?>
