7c94c29a830ccda7f8c3aa772fa919a8566c5c5d
Victor C.B. <victor.charneco.barba@gmail.com>
2025-11-17 20:24:57 +0100

    Buscador class created
    as per SOLID unique responsability

---
+++ Buscador.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once("Vuelo.php");
+
+class Buscador{
+    
+    public function vueloMasBarato(array $vuelos): ?Vuelo{
+        if(empty($vuelos)){
+            return null;
+        }
+        $vueloChollo= $vuelos[0];
+        foreach($vuelos as $vuelo){
+            if($vuelo->getPrecio() < $vueloChollo->getPrecio()){
+                $vueloChollo = $vuelo;
+            }
+        }
+        return $vueloChollo;
+    }
+
+    public function vueloPorDestino(array $vuelos, string $destino):array{
+        if(empty($vuelos)){
+            return [];
+        }
+        $resultado = [];
+        foreach($vuelos as $vuelo){
+            if(strtolower($vuelo->getDestino()) === strtolower($destino)){
+                $resultado[] = $vuelo;
+            }
+        }
+        return $resultado;
+    }
+}
+
+?>
