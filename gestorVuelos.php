3fd900a7ca399704dfa223f958e819d3362b2c4a
Victor C.B. <victor.charneco.barba@gmail.com>
2025-11-17 20:22:03 +0100

    updated to a properly class name

--- gestorVuelos.php
+++ gestorVuelos.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once ("Vuelo.php");
-require_once ("funcionesVuelo.php");
+require_once ("Buscador.php");
 require_once ("creacionVuelos.php");
 require_once ("busquedasDeVuelo.php");
 
